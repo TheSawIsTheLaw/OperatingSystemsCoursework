1493d29d3ed46419f54345e815c63e16cb99b422
trvehazzk3r <dimakrok@bk.ru>
2021-09-26 15:22:22 +0300

    ADD: new style for this lab

--- fromLab03/md.c
+++ fromLab03/md.c
@@ -1,33 +1,29 @@
-#include <linux/module.h> 
-#include <linux/kernel.h>
-#include <linux/init_task.h>
 #include <linux/init.h>
+#include <linux/init_task.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yakuba D.");
 
-
-static int __init md_init(void) 
+static int __init md_init(void)
 {
-   printk(KERN_INFO "Module init:\n");
+    printk(KERN_INFO "Module init:\n");
 
-   struct task_struct *task = &init_task;
-   do
-   {
-       printk(KERN_INFO "~~~~~~ Proccess: %s (comm) - %d (ID), Parent: %s (comm) - %d (ID)\n", 
-            task->comm, task->pid, task->parent->comm, task->parent->pid);
-   } while ((task = next_task(task)) != &init_task);
+    struct task_struct *task;
+    task = &init_task;
+    for_each_process(task)
+    {
+        printk(KERN_INFO "~~[TASK INFO]~~ name: %s, priority: %d", task->comm, task->prio);
+    }
 
-   printk(KERN_INFO "~~~~~~ Proccess: %s (comm) - %d (ID), Parent: %s (comm) - %d (ID)\n",
-		current->comm, current->pid, current->parent->comm, current->parent->pid);
-
-   return 0; 
+    return 0;
 }
 
-static void __exit md_exit(void) 
-{ 
-   printk(KERN_INFO "Module goes away... It's his final message. If you won't replace the battery...\n"); 
-} 
+static void __exit md_exit(void)
+{
+    printk(KERN_INFO "Module goes away... It's his final message. If you won't replace the battery...\n");
+}
 
-module_init(md_init); 
-module_exit(md_exit); 
+module_init(md_init);
+module_exit(md_exit);
