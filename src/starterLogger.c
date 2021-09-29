6293bc5ce3dffa63b23f74cbe77ac245d55d71ae
trvehazzk3r <dimakrok@bk.ru>
2021-09-29 15:59:46 +0300

    ADD: real-time output

--- src/starterLogger.c
+++ src/starterLogger.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define SIZE_OF_DMESG_OUTPUT 8192
 
 #define ERROR_COMMAND_EXEC 1
+
+#define TIMES 15
+#define DELAY 10
 
 #define MODULE_STRING_UNIQUE "~~[TASK INFO]~~: procID: "
 
 int main(int argc, char *argv[])
 {
     FILE *filePointer = NULL;
-    char dmesg[SIZE_OF_DMESG_OUTPUT] = { '\0' };
+    char dmesg[SIZE_OF_DMESG_OUTPUT] = {'\0'};
 
-    pclose(popen("sudo dmesg -c", "r"));
-    filePointer = popen("sudo dmesg", "r");
-
-    if (filePointer == NULL)
+    for (int i = 0; i < TIMES; i++)
     {
-        printf("Error: can't execute dmesg");
-        return ERROR_COMMAND_EXEC;
-    }
+        filePointer = popen("sudo dmesg -c", "r");
 
-    char checkString[512] = { "\0" };
-    if (argv[1])
-        snprintf(checkString, sizeof(checkString), "%s%s%s", MODULE_STRING_UNIQUE, argv[1], ",");
-
-    while(fgets(dmesg, sizeof(dmesg), filePointer) != NULL)
-    {
-        if (checkString[0] == '\0' || strstr(dmesg, checkString))
+        if (filePointer == NULL)
         {
-            printf("%s", dmesg);
+            printf("Error: can't execute dmesg");
+            return ERROR_COMMAND_EXEC;
         }
-    }
 
-    pclose(filePointer);
+        char checkString[512] = {"\0"};
+        if (argv[1])
+            snprintf(checkString, sizeof(checkString), "%s%s%s", MODULE_STRING_UNIQUE, argv[1], ",");
+
+        while (fgets(dmesg, sizeof(dmesg), filePointer) != NULL)
+        {
+            if (checkString[0] == '\0' || strstr(dmesg, checkString))
+            {
+                printf("%s", dmesg);
+            }
+        }
+
+        pclose(filePointer);
+        sleep(DELAY);
+    }
 
     return EXIT_SUCCESS;
 }
