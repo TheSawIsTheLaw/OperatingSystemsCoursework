7e60144ac04ac5e45dcc307ec230e3da85df66fc
trvehazzk3r <dimakrok@bk.ru>
2021-09-30 23:24:45 +0300

    ADD: scheduler and rt info

--- src/md.c
+++ src/md.c
@@ -1,8 +1,8 @@
+#include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/init_task.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/delay.h>
 
 #include <linux/kthread.h>
 
@@ -22,8 +22,13 @@ static int printTasks(void *arg)
         printk(KERN_INFO "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~: %lu TIME", currentPrint);
         for_each_process(task)
         {
-            printk(KERN_INFO "~~[TASK INFO]~~: procID: %5d, name: %15s, priority: %4d, delay: %30lld, runs: %30lld (ticks)", task->pid, task->comm, task->prio,
-                   task->sched_info.run_delay, task->utime);
+            printk(KERN_INFO "~~[TASK INFO]~~: procID: %5d, name: %15s, prio: %4d, static_prio: %d, normal_prio (with "
+                             "scheduler policy): %d, realtime_prio: %d "
+                             "delay: %30lld, utime: %30lld (ticks), stime: %15d (ticks)",
+                   task->pid, task->comm, task->prio, task->static_prio, task->normal_prio, task->rt_priority,
+                   task->sched_info.run_delay, task->utime, task->stime);
+            printk(KERN_INFO "Sched_rt_entity: timeout: %ld, watchdog_stamp: %ld, time_slice: %ld", task->rt.timeout,
+                   task->rt.watchdog_stamp, task->rt.time_slice);
         }
 
         currentPrint++;
