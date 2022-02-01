98e78c04c5b5da652d86c9186ee5c977e69a3331
trvehazzk3r <dimakrok@bk.ru>
2022-02-01 18:47:04 +0300

    ADD: last info in analysis i hope

---
+++ doc/RPZ/code/copy_to_user.h
@@ -0,0 +1,7 @@
+static __always_inline unsigned long __must_check
+copy_to_user(void __user *to, const void *from, unsigned long n)
+{
+	if (likely(check_copy_size(from, n, true)))
+		n = _copy_to_user(to, from, n);
+	return n;
+}
